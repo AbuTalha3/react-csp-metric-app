7e21a84d1a366f1bebbf2702291fb17f0a1b4358
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-10-19 22:35:30 +0500

    Amend CSS files

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
   return (
     <div className="stock-list-container">
       <nav className="stock-list-navbar">
-        <h1 className="stock-list-heading"> Ben Stocks </h1>
+        <h1 className="stock-list-heading"> BEN STOCKS </h1>
         <div className="stock-list-searchbar">
           <label htmlFor="search">
             Search
